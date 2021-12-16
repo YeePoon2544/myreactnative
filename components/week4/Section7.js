1a5d612c5dfe172f347fe38b2ce7e789ecdde0bf
yeepoon <94604445+YeePoon2544@users.noreply.github.com>
2021-12-16 18:13:13 +0700

    week5

--- components/week4/Section7.js
+++ components/week4/Section7.js
@@ -4,12 +4,12 @@ import { ScrollView, StyleSheet, Text, View, TextInput, Button, Image, } from 'r
 
 export default function Section7 () {
     return (
-        <View style={{ flex : 1  ,margin : 20 , marginTop : 5 , borderWidth : 1 , borderColor : "white" , paddingBottom : 10 }}>
+        <View style={{ flex : 1  , margin : 20 , marginTop : 5 , borderWidth : 1 , borderColor : "white" , paddingBottom : 10 , borderBottomColor: "#A9A9A9" }}>
              <View>
                 <Text style={{fontSize : 15}}>Room Type</Text>
             </View>
             <View style={{ flexDirection : "row", padding : 10}}> 
-                <Image style={{width :60 , height : 60 , borderRadius : 40/3 }} source={require('../../assets/week3/room-8.jpg')} />
+                <Image style={{width :60 , height : 60 , borderRadius : 40/2 }} source={require('../../assets/week3/room-8.jpg')} />
                 <View style={{ paddingLeft : 10}}>
                     <Text >Standard Twin Room</Text>
                     <Text style={{ color: "red" , marginTop :8}}>$399.99</Text>
